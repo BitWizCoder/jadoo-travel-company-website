19f501c0102538c67a252345756ab10ccc96bfdc
Muhammad Noman <mhmdnoman01@gmail.com>
2025-08-13 00:28:16 +0600

    updated hearo and nav

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,24 +1,35 @@
+import { PlayIcon } from "@phosphor-icons/react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="flex justify-between items-center">
-      <div className="w-1/2">
-        <h2 className="text-xl uppercase text-[#DF6951]">
+    <div className="text-center mt-14 flex flex-col items-center">
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-lg uppercase text-[#DF6951] font-bold">
           Best Destinations around the world
         </h2>
         <h1 className="text-7xl">
           Travel, enjoy <br /> and live a new <br /> and full life
         </h1>
-        <p>
+        <p className="mx-w-xl px-6">
           Built Wicket longer admire do barton vanity itself do in it. Preferred
           to sportsmen it engrossed listening. Park gate sell they west hard for
           the.
         </p>
         <div className="flex gap-6">
-          <Link href="/">Find out more</Link>
-          <Link href="/">Play Demo</Link>
+          <Link
+            href="/"
+            className="bg-[#F1A501] text-white py-4 px-6 rounded-xl"
+          >
+            Find out more
+          </Link>
+          <Link href="/" className="flex items-center gap-4">
+            <div className="bg-[#DF6951] rounded-full p-3">
+              <PlayIcon size={32} className="text-white" />
+            </div>
+            Play Demo
+          </Link>
         </div>
       </div>
       <div>
